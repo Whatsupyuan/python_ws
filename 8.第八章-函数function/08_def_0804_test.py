abc2d0d135b0f9d9e1fda839b3ea864edb140a2a
Whatsupyuan <hryuan911@qq.com>
2018-01-11 13:46:23 +0800

    add code to 第八章

--- 8.第八章-函数function/08_def_0804_test.py
+++ 8.第八章-函数function/08_def_0804_test.py
@@ -10,7 +10,7 @@ make_shirt_two(str(32))
 make_shirt_two(size=str(32))
 
 # 如果调用函数时 , 如果有一个参数为绑定参数的方式调用,则之后的也必须为绑定参数
-# 下一行被注释代码报错
+# 下一行被注释代码执行会报错 , 因为没有指定 第二个参数的绑定 导致
 # make_shirt_two(size=str(27) , "nike")
 
 # 8-5
---
+++ 8.第八章-函数function/08_def_0805_returnTest.py
@@ -0,0 +1,36 @@
+# 8-6
+def city_country(city , country , songs=""):
+    print(city + "," + country)
+city_country("Beijing","China" , "01-Cross")
+city_country("LA","American")
+city_country("CLC","American")
+
+# 8-7
+def make_album(singer , albumName):
+    return {singer : albumName}
+def make_album2(singer , albumName):
+    dic = {}
+    dic[singer] = albumName
+    return dic
+
+a1 = make_album("akon" , "will")
+a2 = make_album("Lady","Proker Face")
+a3 = make_album2("akon" , "will")
+a4 = make_album2("Lady","Proker Face")
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+
+# 8-9
+flag = True
+while flag:
+    albumInfo = {}
+    albumName = input("Please input your album name,If want exit please input 'q'")
+    singer = input("Please input your singer name,If want exit please input 'q'")
+    if albumName == 'q' or singer == 'q':
+        break
+    albumInfo = make_album2(singer,albumName)
+    print(albumInfo)
+
+
