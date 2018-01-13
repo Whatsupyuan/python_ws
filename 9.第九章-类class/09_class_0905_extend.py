76a46c9dabbec67a44c8f045571fb9e1c41689fc
hryuan911 <hryuan911@qq.com>
2018-01-13 23:44:34 +0800

    继承01 - add

---
+++ 9.第九章-类class/09_class_0905_extend.py
@@ -0,0 +1,22 @@
+class Car():
+    def __init__(self , name , series):
+        self.name = name
+        self.series = series
+
+    def carInfo(self):
+        print(self.name + " " + self.series)
+
+
+class ElectriCar(Car):
+    def __init__(self , name , series ):
+        # super() 必须写成方法体形态 , 与JAVA不同
+        super().__init__(name , series)
+
+    def printElectriCarInfo(self):
+        print(self.name + " " + self.series + "Electri")
+
+car = Car("toyota" , "霸道")
+car.carInfo()
+
+carElec = ElectriCar("特斯拉" , "P80")
+carElec.printElectriCarInfo()
