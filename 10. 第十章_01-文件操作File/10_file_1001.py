3b766d11079b64bce8cc74ed4884017e633e1bdc
Whatsupyuan <hryuan911@qq.com>
2018-01-15 11:07:12 +0800

    第九章 create

--- 10. 第十章_01-文件操作File/10_file_1001.py
+++ 10. 第十章_01-文件操作File/10_file_1001.py
@@ -1,3 +1,6 @@
-with open("/Users/hryuan911/develop/1.txt") as fileTxt:
+# 读取不同目录下文件内容
+#with open("/Users/hryuan911/develop/1.txt") as fileTxt:
+# 本地文件夹下
+with open("1.txt") as fileTxt:
     read = fileTxt.read()
     print(read.rstrip())
