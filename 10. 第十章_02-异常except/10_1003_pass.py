e1b65ab0e8c05b1f3f5b1d606b6e79ba0f4aa51d
Whatsupyuan <hryuan911@qq.com>
2018-01-17 10:27:41 +0800

    第十章 add code

--- 10. 第十章_02-异常except/10_1003_pass.py
+++ 10. 第十章_02-异常except/10_1003_pass.py
@@ -14,6 +14,20 @@ def countWord(fileName):
                 wordsCount += len(line.split())
             return wordsCount
 
+# 第二种方法: 一次性读取所有文件内容
+def countFileIncludWord(fileName):
+    if fileName:
+        try:
+            with open(fileName) as file:
+                content = file.read()
+        except:
+            pass
+        else:
+            return len(content.split())
+
 num = countWord("10_1001_exception.py")
+num2 = countFileIncludWord("10_1001_exception.py")
 if num:
     print(num)
+if num2:
+    print(num2)
