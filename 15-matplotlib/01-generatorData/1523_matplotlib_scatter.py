c6419595291b924eea545bc9ee2b96edb2b68d7c
Whatsupyuan <hryuan911@qq.com>
2018-01-30 11:59:33 +0800

    add code

--- 15-matplotlib/01-generatorData/1523_matplotlib_scatter.py
+++ 15-matplotlib/01-generatorData/1523_matplotlib_scatter.py
@@ -1,4 +1,13 @@
 import matplotlib.pyplot as pf
 
-pf.scatter([2, 4], [4, 8],[8,16])
+rangeList = range(1,11)
+print("rangList 类型: " + str(type(rangeList)))
+xList = [x for x in range(10)]
+yList = [y*y for y in range(10)]
+
+print("xList 类型: " + str(type(xList)))
+# xList = [1, 2, 3, 4, 5]
+# yList = [1, 4, 9, 16, 25]
+
+pf.scatter(xList , yList , s=100)
 pf.show()
