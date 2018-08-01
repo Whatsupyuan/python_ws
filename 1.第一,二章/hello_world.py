bff4dc2f09aeb1b97fbb28b86e165fd74dcbe5ac
hryuan911 <hryuan911@qq.com>
2018-08-01 17:13:50 +0800

    去除不规范的特殊符号

--- 1.第一,二章/hello_world.py
+++ 1.第一,二章/hello_world.py
@@ -1,7 +1,7 @@
-print("Hello") ; 
+print("Hello")
 
-info1="info1 hello 01";
-print(info1) ; 
-info2="info1 hello 02";
-print(info2) ;
-print(ifnl) ; 
+info1="info1 hello 01"
+print(info1) 
+info2="info1 hello 02"
+print(info2) 
+print(ifnl)  
--- 1.第一,二章/name_01.py
+++ 1.第一,二章/name_01.py
@@ -1,8 +1,8 @@
-message="ada name" ; 
-print(message.title());
+message="ada name"  
+print(message.title())
 
-message=message+" CCC" ; 
-print(message.upper()) ; 
+message=message+" CCC"  
+print(message.upper())  
 
-message = message + " Ddd" ; 
-print(message.lower());
+message = message + " Ddd"  
+print(message.lower())
